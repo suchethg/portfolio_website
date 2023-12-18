4c5a109227ab8122b4ac34cb243b1d30a5aabb2d
sucheth19 <130819674+sucheth19@users.noreply.github.com>
2023-12-17 23:29:12 -0800

    first

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -5,5 +5,5 @@ export const headerData = {
     title: "Software Engineer, Full Stack Developer, and an Cloud Enthusiast",
     desciption:"I am a self-motivated, creative and technically sound computer/software engineer with a keen interest in learning new technologies to build enterprise applications. Capable of functioning both in a team and on one's own. I am consummate in the fields of Enterprise Software Development, Web Development, and Cloud Computing. ",
     image: 'https://raw.githubusercontent.com/PhantomScript/asset-container/main/developer-portfolio/landingImg.png',
-    resumePdf: resume
+    resumePdf: "https://drive.google.com/file/d/1PgRsA-3qZaZHQmE4yaMVYDbpnRCF7QM4/view?usp=sharing"
 }
